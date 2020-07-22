534e8f3a6ebfa49531c2dbf3343f968a7b779c29
slothdabski <allthatsleft169@gmail.com>
2020-07-22 18:07:32 -0500

    oneplus7pro: Adapt overlays for PixelDust

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay-pd
 
 ifneq ($(TARGET_IS_ONEPLUS_T_DEVICE),true)
 DEVICE_PACKAGE_OVERLAYS += \
